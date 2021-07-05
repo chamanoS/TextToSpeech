5f122ac786f2b4ee201c883c255e4d343879f3af
Chamano Ramashia <chamano.sr@gmail.com>
2021-07-05 18:41:12 +0200

    changing text

--- project/project.py
+++ project/project.py
@@ -1,14 +1,9 @@
-# use google text to speech package(api)
-#pip and latest python version
 from gtts import gTTS
 import os
 
 fh = open("text.txt", "r")
-#replace(), replaces the line endings
-myText = fh.read().replace("\n", "")
-
+myText = fh.read().replace("\n","")
 language = 'en'
-#make a request
 output = gTTS(text=myText, lang=language, slow=False)
 
 output.save("myOutput.mp3")
